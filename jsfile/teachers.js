4f4b0a8e276238a6cefb0b2c881c1bc58423b037
code-mango-graham <arnold.dc.it@gmail.com>
2026-06-07 09:00:03 -0400

    Mac update

--- jsfile/teachers.js
+++ jsfile/teachers.js
@@ -1,6 +1,32 @@
    
    $(document).ready(function () {
        $('#teachersTable').DataTable({
-            scrollX: true
+           layout: {
+        topStart: {
+
+    buttons: [
+        {
+            extend: 'copy',
+            text: 'Copy',
+            className: 'btn btn-secondary btn-sm'
+        },
+        {
+            extend: 'excel',
+            text: 'Excel',
+            className: 'btn btn-success btn-sm'
+        },
+        {
+            extend: 'pdf',
+            text: 'PDF',
+            className: 'btn btn-danger btn-sm'
+        },
+        {
+            extend: 'print',
+            text: 'Print',
+            className: 'btn btn-primary btn-sm'
+        }
+    ]
+        }
+    }
         });
     });
