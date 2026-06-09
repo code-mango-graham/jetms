32bb2ff48535f2cc407731a332e7167c8025f6c2
code-mango-graham <arnold.dc.it@gmail.com>
2026-06-09 01:16:46 -0400

    updated data tables

--- jsfile/teachers.js
+++ jsfile/teachers.js
@@ -1,36 +1,18 @@
    
    $(document).ready(function () {
-
-        $('#teachersTable').DataTable({
+        var teachersTable = $('#teachersTable').DataTable({
             scrollX: true,
             language: {
                 lengthMenu: "Show _MENU_ entries"
             },
             layout: {
                 topStart: [
-                    
                     {
                         buttons: [
-                            {
-                                extend: 'copy',
-                                text: 'Copy',
-                                className: 'btn btn-secondary btn-sm'
-                            },
-                            {
-                                extend: 'excel',
-                                text: 'Excel',
-                                className: 'btn btn-success btn-sm'
-                            },
-                            {
-                                extend: 'pdf',
-                                text: 'PDF',
-                                className: 'btn btn-danger btn-sm'
-                            },
-                            {
-                                extend: 'print',
-                                text: 'Print',
-                                className: 'btn btn-primary btn-sm'
-                            }
+                            { extend: 'copy', text: 'Copy', className: 'btn btn-secondary btn-sm' },
+                            { extend: 'excel', text: 'Excel', className: 'btn btn-success btn-sm' },
+                            { extend: 'pdf', text: 'PDF', className: 'btn btn-danger btn-sm' },
+                            { extend: 'print', text: 'Print', className: 'btn btn-primary btn-sm' }
                         ]
                     },
                     'pageLength'
